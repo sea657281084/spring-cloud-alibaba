b4784d561d2e3ebe6907a2cf194c13b2ed868666
SEA <657281084@qq.com>
2022-10-24 15:16:46 +0800

    初始化 cloud-api-commons

---
+++ cloud-api-commons/src/main/java/pojo/CommonResult.java
@@ -0,0 +1,28 @@
+package pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * @ClassName CommonResult
+ * @Description TODO
+ * @Author LHQ
+ * @Date 2022/10/18 15:09
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class CommonResult<T> {
+
+    private Integer code;//返回状态码
+    private String message;//返回是否调用成功
+    private T data; //返回的数据
+
+    public CommonResult(Integer code, String message) {
+        this(code, message, null);
+    }
+}
