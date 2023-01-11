6bb6526fe638b7ebfb22ddcf9febe0b9a6c467b0
SEA <657281084@qq.com>
2023-01-11 17:52:06 +0800

    解决了mybatis的mapper和xml方法绑定问题

--- cloud-sentinel-config/src/main/java/com/buba/sentinel/controller/FlowLimitController.java
+++ cloud-sentinel-config/src/main/java/com/buba/sentinel/controller/FlowLimitController.java
@@ -1,4 +1,4 @@
-package com.study.sentinel.controller;
+package com.buba.sentinel.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
